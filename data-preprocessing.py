6f53985ce0c0c4a8c54628c4e6b0c3483ed7d4c8
rbrueda <rbrueda8@gmail.com>
2024-08-29 15:30:55 -0400

    wrote clusters into a csv file

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -1,26 +1,27 @@
+
+#todo: find the dimension length from the data -- find the most optimal scale number using a mathematical equation
+#todo: figure out the most optimal min_count number -- this value probably works best with 1, as we want all values to produce some unique value -- but im not sure how it will behave with "dance pop" versus "pop" -- similar words
+
+#todo: figure out issue and propose solution 
+#todo: goal - finish this part tonight so i can move onto different playlist cateogries --> popularity, release dates, artists name (should be simular to word to vec? -- but i think it isnt really -- we need to find the most popular)
+
 import pandas as pd
-from nltk.corpus import stopwords
 import re
+import numpy as np
+
 import nltk
-import numpy
+from nltk.corpus import stopwords
 
 # import word-2-vec model
 from gensim.models import Word2Vec
 
+#for visualizing data
+import streamlit as st
+import altair as alt
+
+#for clustering the data
 from sklearn.decomposition import PCA
-
-
-# #function to preprocess the data
-# def preprocess(text):
-#     #remove any characters that are not alphanumeric
-#     text_input = re.sub('[^a-zA-Z1-9&,]+', ' ', str(text))
-#     output = re.sub(r'\d+', '',text_input)
-#     return output.lower().strip()
-
-# #function to remove stopword in the data
-# def remove_stopwords(text):
-#     filtered_words = [word.lower() for word in text.split() if word.lower() not in stop]
-#     return " ".join(filtered_words)
+from sklearn.cluster import KMeans
 
 #function to create a collection of data
 def build_collection(data):
@@ -41,8 +42,6 @@ def build_collection(data):
 
 df = pd.read_csv('sampleSongs.csv')
 
-#* albumType -> maybe? (options: single or album)
-
 #here will list the columns to find relevant information from 
 metricColumns = ['artistName', 'albumPopularity', 'albumReleaseDate', 'artistGenres']
 
@@ -54,14 +53,8 @@ nltk.download('stopwords')
 #gets all the stopwords in english
 stop = set(stopwords.words("english"))
 
-# sub_df['artistGenres'] = sub_df.artistGenres.map(preprocess)
-# sub_df['artistGenres'] = sub_df.artistGenres.map(remove_stopwords)
-
 #create a collection of words from album genres
 collection_of_words = build_collection(sub_df['artistGenres'])
-
-#todo: find the dimension length from the data -- find the most optimal scale number using a mathematical equation
-#todo: figure out the most optimal min_count number -- this value probably works best with 1, as we want all values to produce some unique value -- but im not sure how it will behave with "dance pop" versus "pop" -- similar words
 
 #train a word2vec model
 model = Word2Vec(collection_of_words, vector_size=20, min_count=1)
@@ -79,20 +72,56 @@ pca = PCA(n_components=2)
 #fit the vectors in the PCA object created -- used for dimensionality reduction
 PCA_result = pca.fit_transform(vectors)
 
-#create a dataframe for the list of vectors
-words = pd.DataFrame(words)
 
-#convert PCA to dataframe format
-PCA_result = pd.DataFrame(PCA_result)
+#this is what we will start with for the number of clusters -- the number of broad categories
+genres = ["Hip Hop", "Pop", "Rock", "R&B", "Rap", "Country", "K-Pop", "Lo-fi", "Indie", "Metal", "Punk", "Jazz", "Classical"]
 
-PCA_result['x_values'] = PCA_result.iloc[0:, 0]
-PCA_result['y_values'] = PCA_result.iloc[0:, 1]
+kmeans = KMeans(n_clusters=len(genres))
 
-PCA_final = pd.merge(words, PCA_result, left_index=True, right_index=True)
-PCA_final['word'] = PCA_final.iloc[0:, 0]
+#fit K-means model with k=5
+kmeans.fit(PCA_result)
 
-PCA_data_complet = PCA_final[['word', 'x_values', 'y_values']]
+#algorithm used for parititioning a dataset into a pre-defined number of clusters
+clusters = kmeans.predict(PCA_result)
 
-print(PCA_data_complet)
+#dataframe for PCA results and clusters
+PCA_df = pd.DataFrame(PCA_result, columns=['x_values', 'y_values'])
+PCA_df['word'] = model.wv.index_to_key
+PCA_df['cluster'] = clusters
 
-#todo: plot data on streamlit and look at behavior --- may need to scale the data
+#look at the result of PCA dataframe
+st.write(PCA_df)
+
+#not sure how time consuming this is but ill do it like this: search for word in dataframe and add it to the cluster value
+
+#split the list of artistGenres from sub_df into lists of individual genres (in order to perform merge with PCA_df)
+sub_df['artistGenres'] = sub_df['artistGenres'].str.split(', ')
+
+#explode the list from sub_df into separate rows
+sub_df_exploded = sub_df.explode('artistGenres')
+
+st.write(sub_df_exploded)
+
+#merge the exploded dataframe with PCA_df for the artist genre
+df_merged_genre = pd.merge(sub_df_exploded, PCA_df, left_on='artistGenres', right_on='word', how='left')
+
+st.write(df_merged_genre)
+
+#iterate through each line of the cluster and print to a specific csv file
+for index, entry in df_merged_genre.iterrows():
+    # Check if the 'cluster' value is not NaN
+    if pd.notna(entry['cluster']):
+        # Open the file for writing
+        with open('samples/clusterRes' + str(entry['cluster']) + '.csv', 'a') as f:
+            # Convert the row (entry) to a DataFrame
+            entry_df = entry.to_frame().T
+            
+            # Check if the file is empty to write the header
+            write_header = f.tell() == 0
+            
+            # Write the DataFrame to the CSV file
+            entry_df.to_csv(f, index=False, header=write_header)
+
+
+#iterate through all the csv files and append the values to a set and write the results to the console
+
